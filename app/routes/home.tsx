3d1dfc36e066e59fb74344c0e15b3098918ad411
siddheshtodkar <stodkar53@gmail.com>
2025-08-15 18:24:42 +0530

    project setup with devtools error fix"

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Freindly Dev" },
+    { name: "description", content: "Portfolio Website" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>My App</>;
 }
